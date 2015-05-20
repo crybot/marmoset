e8d2a4a77818b5c499bc8ef2c1eabadbe0046f07
Marco Pampaloni <crybot@hotmail.it>
2015-05-20 19:21:54 +0200

    modelled user for authorization system

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150517205237) do
+ActiveRecord::Schema.define(version: 20150520171232) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "username"
     t.string   "password"
     t.string   "email"
     t.text     "name"
