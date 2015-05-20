2661606cb256f7fa33a81ce22e609451418bea92
Marco Pampaloni <crybot@hotmail.it>
2015-05-20 23:56:18 +0200

    structuring signup page and database user registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,21 @@ class UsersController < ApplicationController
   def index
    @user_list = User.all 
   end
+
   def new
+    @user = User.new
     #    User.create(username: random_string, password: random_string, email: random_string, name: random_string, surname: random_string)
     #    redirect_to users_path
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+    redirect_to users_path
   end
 
   def random_string
@@ -12,5 +24,9 @@ class UsersController < ApplicationController
   end
 
   def find_user
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :surname, :email, :password)
   end
 end
