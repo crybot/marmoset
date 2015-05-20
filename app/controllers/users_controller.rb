c4613905e0e78f7bb89c919785cddcb22353fa70
Marco Pampaloni <crybot@hotmail.it>
2015-05-20 14:55:31 +0200

    heroku deploy test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,8 @@ class UsersController < ApplicationController
    @user_list = User.all 
   end
   def new
-    User.create(username: random_string, password: random_string, email: random_string, name: random_string, surname: random_string)
-    redirect_to users_path
+    #    User.create(username: random_string, password: random_string, email: random_string, name: random_string, surname: random_string)
+    #    redirect_to users_path
   end
 
   def random_string
