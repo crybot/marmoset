aa6f0144476221a8f40c7d7f2eb55590a217beed
Marco Pampaloni <crybot@hotmail.it>
2015-05-21 18:11:13 +0200

    improved registration system - now handling input errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,32 +1,32 @@
 class UsersController < ApplicationController
   def index
-   @user_list = User.all 
+    @user_list = User.all 
   end
 
   def new
     @user = User.new
-    #    User.create(username: random_string, password: random_string, email: random_string, name: random_string, surname: random_string)
-    #    redirect_to users_path
   end
-  
+
   def show
     @user = User.find(params[:id])
   end
 
   def create
     @user = User.new(user_params)
-    @user.save
-    redirect_to users_path
+    if @user.save
+      redirect_to users_path
+    else
+      render new_user_path
+    end
   end
 
-  def random_string
-     ('a'..'z').to_a.shuffle[0..10].join
-  end
+  #def random_string
+  #('a'..'z').to_a.shuffle[0..10].join
+  #end
 
-  def find_user
-  end
-
+  private
   def user_params
     params.require(:user).permit(:name, :surname, :email, :password)
   end
+
 end
