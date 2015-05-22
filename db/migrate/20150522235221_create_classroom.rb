f305440115b0b00e74e8c2e39c676fe4a9aecad2
Marco Pampaloni <crybot@hotmail.it>
2015-05-23 01:57:12 +0200

    updated migration file for heroku

--- db/migrate/20150522235221_create_classroom.rb
+++ db/migrate/20150522235221_create_classroom.rb
@@ -1,11 +1,5 @@
 class CreateClassroom < ActiveRecord::Migration
   def change
-    create_table :classrooms  do |t|
-      t.text :name
-
-      t.timestamps null: false
-    end
-
     create_table :classrooms_users, id: false  do |t|
       t.belongs_to :user, index: true
       t.belongs_to :classroom, index: true
